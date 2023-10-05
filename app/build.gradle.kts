3da2426d99c8eee8ddbc11912e42dbafed846b02
cuevasanta <aacuevasanta@gmail.com>
2023-10-05 21:31:42 +0200

    Add espresso version const

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,11 +68,13 @@ dependencies {
     implementation(Compose.material3)
 
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    testImplementation(Testing.junit4)
+    testImplementation(Testing.junitAndroidExt)
+    testImplementation(Testing.junitAndroidExt)
+
+    androidTestImplementation(platform(Compose.composeBom))
+    androidTestImplementation(Testing.composeUiTest)
+    androidTestImplementation(Testing.espresso)
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- buildSrc/src/main/java/Compose.kt
+++ buildSrc/src/main/java/Compose.kt
@@ -6,20 +6,10 @@ object Compose {
     const val uiGrapichs = "androidx.compose.ui:ui-graphics"
     const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview"
     const val material3 = "androidx.compose.material3:material3"
-
-    const val compiler = "androidx.compose.compiler:compiler"
-
-    //private const val navigationVersion = "2.4.0-beta02"
-    const val navigation = "androidx.navigation:navigation-compose"
-
-    //private const val hiltNavigationComposeVersion = "1.0.0-beta01"
-    const val hiltNavigationCompose = "androidx.hilt:hilt-navigation-compose"
-
-    //private const val activityComposeVersion = "1.4.0"
+    //const val compiler = "androidx.compose.compiler:compiler"
+    //const val navigation = "androidx.navigation:navigation-compose"
+    //const val hiltNavigationCompose = "androidx.hilt:hilt-navigation-compose"
     const val activityCompose = "androidx.activity:activity-compose"
-
-    //private const val lifecycleVersion = "2.6.2"
     const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose"
-
-    const val runtime = "androidx.compose.runtime:runtime"
+    //const val runtime = "androidx.compose.runtime:runtime"
 }
