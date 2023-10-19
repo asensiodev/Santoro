0354670e0e5c7abc61e51e93cd95765a0bed204f
cuevasanta <aacuevasanta@gmail.com>
2023-10-19 20:38:30 +0200

    Fix modules typo

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -4,15 +4,15 @@ object Modules {
     const val core = ":core"
     const val coreUi = ":core_ui"
 
-    const val onboardingData = ":onboarding:data"
-    const val onboardingDomain = ":onboarding:domain"
-    const val onboardingPresentation = ":onboarding:presentation"
+    const val onboardingData = ":onboarding:onboarding_data"
+    const val onboardingDomain = ":onboarding:onboarding_domain"
+    const val onboardingPresentation = ":onboarding:onboarding_presentation"
 
-    const val loginData = ":login:data"
-    const val loginDomain = ":login:domain"
-    const val loginPresentation = ":login:presentation"
+    const val loginData = "login:login_data"
+    const val loginDomain = "login:login_domain"
+    const val loginPresentation = "login:login_presentation"
 
-    const val movieListData = ":movie_list:data"
-    const val movieListDomain = ":movie_list:domain"
-    const val movieListPresentation = ":movie_list:presentation"
+    const val movieListData = "movie_list:movie_list_data"
+    const val movieListDomain = "movie_list:movie_list_domain"
+    const val movieListPresentation = "movie_list:movie_list_presentation"
 }
