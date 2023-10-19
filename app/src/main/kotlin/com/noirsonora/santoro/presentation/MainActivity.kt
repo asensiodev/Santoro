585828e630c3d5f74f54616a7e43939c845c68aa
cuevasanta <aacuevasanta@gmail.com>
2023-10-19 20:39:38 +0200

    Navigate to welcome screen

--- app/src/main/kotlin/com/noirsonora/santoro/presentation/MainActivity.kt
+++ app/src/main/kotlin/com/noirsonora/santoro/presentation/MainActivity.kt
@@ -3,13 +3,12 @@ package com.noirsonora.santoro.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.noirsonora.core.navigation.Route
+import com.noirsonora.onboarding_presentation.welcome.WelcomeScreen
+import com.noirsonora.santoro.presentation.navigation.navigate
 import com.noirsonora.santoro.presentation.ui.theme.SantoroTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +16,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SantoroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.WELCOME
                 ) {
-                    Greeting("Android")
+                    composable(Route.WELCOME) {
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.LOGIN) {
+
+                    }
+                    composable(Route.MOVIE_LIST) {
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SantoroTheme {
-        Greeting("Android")
     }
 }
