c36f281669fb4aef3d649e451a753e6641315605
cuevasanta <aacuevasanta@gmail.com>
2023-10-24 20:59:17 +0200

    Fix module naming

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -4,11 +4,11 @@ object Modules {
     const val core = ":core"
     const val coreUi = ":core_ui"
 
-    const val loginData = "login:login_data"
-    const val loginDomain = "login:login_domain"
+    const val loginData = ":login:login_data"
+    const val loginDomain = ":login:login_domain"
     const val loginPresentation = ":login:login_presentation"
 
-    const val movieListData = "movie_list:movie_list_data"
-    const val movieListDomain = "movie_list:movie_list_domain"
-    const val movieListPresentation = "movie_list:movie_list_presentation"
+    const val movieListData = ":movie_list:movie_list_data"
+    const val movieListDomain = ":movie_list:movie_list_domain"
+    const val movieListPresentation = ":movie_list:movie_list_presentation"
 }
