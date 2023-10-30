e7e05a944536967c66d2044ad5dddac3752b00b0
cuevasanta <aacuevasanta@gmail.com>
2023-10-30 20:48:08 +0100

    Splash screen modifications

--- app/src/main/kotlin/com/noirsonora/santoro/MainActivity.kt
+++ app/src/main/kotlin/com/noirsonora/santoro/MainActivity.kt
@@ -3,6 +3,7 @@ package com.noirsonora.santoro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -14,6 +15,7 @@ import com.noirsonora.santoro.ui.theme.SantoroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             SantoroTheme {
