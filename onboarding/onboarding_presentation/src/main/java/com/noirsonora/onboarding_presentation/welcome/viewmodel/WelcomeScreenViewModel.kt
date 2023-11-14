8aaed64628fbd6c4db01c9de39925c8875605ac3
cuevasanta <aacuevasanta@gmail.com>
2023-11-14 23:04:55 +0100

    Add WelcomeScreenViewModel

--- onboarding/onboarding_presentation/src/main/java/com/noirsonora/onboarding_presentation/welcome/viewmodel/WelcomeScreenViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/noirsonora/onboarding_presentation/welcome/viewmodel/WelcomeScreenViewModel.kt
@@ -1,5 +1,19 @@
 package com.noirsonora.onboarding_presentation.welcome.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class WelcomeScreenViewModel {
+
+@HiltViewModel
+class WelcomeScreenViewModel : ViewModel() {
+
+    fun saveOnboardingState(completed: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            //
+        }
+    }
+
 }
