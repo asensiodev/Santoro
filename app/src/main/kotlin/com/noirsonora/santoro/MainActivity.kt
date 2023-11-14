145d1112adad29fd8ecd2a1e43e6bb61e6f50c15
cuevasanta <aacuevasanta@gmail.com>
2023-11-14 22:14:09 +0100

    Remove unused import

--- app/src/main/kotlin/com/noirsonora/santoro/MainActivity.kt
+++ app/src/main/kotlin/com/noirsonora/santoro/MainActivity.kt
@@ -8,10 +8,10 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.noirsonora.core.navigation.Route
+import com.noirsonora.core_ui.ui.theme.SantoroTheme
 import com.noirsonora.login_presentation.LoginScreen
 import com.noirsonora.onboarding_presentation.welcome.WelcomeScreen
 import com.noirsonora.santoro.navigation.navigate
-import com.noirsonora.core_ui.ui.theme.SantoroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
