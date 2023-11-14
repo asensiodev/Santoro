b8235960e7c25203b21edf7ef4fdfa9d7b696812
cuevasanta <aacuevasanta@gmail.com>
2023-11-14 23:05:31 +0100

    Add NavController extension function to popBackStack and navigate

--- app/src/main/kotlin/com/noirsonora/santoro/navigation/NavControllerExtension.kt
+++ app/src/main/kotlin/com/noirsonora/santoro/navigation/NavControllerExtension.kt
@@ -6,3 +6,8 @@ import com.noirsonora.core.util.UiEvent
 fun NavController.navigate(event: UiEvent.Navigate) {
     this.navigate(event.route)
 }
+
+fun NavController.navigateAndPopBackstack(event: UiEvent.Navigate) {
+    this.popBackStack()
+    this.navigate(event.route)
+}
