608790df94066cf624bf415c4f55db09a7f00c54
cuevasanta <aacuevasanta@gmail.com>
2023-11-21 20:19:30 +0100

    Rename onboarding route

--- app/src/main/kotlin/com/noirsonora/santoro/MainActivity.kt
+++ app/src/main/kotlin/com/noirsonora/santoro/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.noirsonora.santoro
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -15,9 +13,9 @@ import com.noirsonora.core.navigation.Route
 import com.noirsonora.core_ui.ui.theme.SantoroTheme
 import com.noirsonora.login_presentation.LoginScreen
 import com.noirsonora.onboarding_presentation.welcome.OnboardingScreen
+import com.noirsonora.onboarding_presentation.welcome.viewmodel.SplashViewModel
 import com.noirsonora.santoro.navigation.navigate
 import com.noirsonora.santoro.navigation.navigateAndPopBackstack
-import com.noirsonora.onboarding_presentation.welcome.viewmodel.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,15 +32,14 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             SantoroTheme {
-                val startDestination by splashViewModel.startDestination
                 val navController = rememberNavController()
                 Surface {
                     NavHost(
                         navController = navController,
-                        startDestination = startDestination
+                        startDestination = splashViewModel.startDestination.value
                     ) {
                         //Log.d("START_DESTINATION", startDestination)
-                        composable(Route.WELCOME) {
+                        composable(Route.ONBOARDING) {
                             OnboardingScreen(onNavigate = navController::navigateAndPopBackstack)
                         }
                         composable(Route.LOGIN) {
