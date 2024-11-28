0de2c4de6eba95f4cff22b5410840133cc069257
asensiodev <asensiodev@gmail.com>
2024-11-28 18:23:40 +0100

    Add KtLint, Detekt and precommit hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,80 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+import java.nio.file.attribute.PosixFilePermissions
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
+}
+
+subprojects {
+    apply {
+        plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("io.gitlab.arturbosch.detekt")
+    }
+
+    ktlint {
+        android.set(true)
+        reporters {
+            reporter(ReporterType.PLAIN)
+            reporter(ReporterType.HTML)
+        }
+        filter {
+            exclude("**/generated/**")
+        }
+    }
+}
+
+detekt {
+    val filesProp = project.findProperty("detektFiles") as String?
+    if (!filesProp.isNullOrBlank()) {
+        val fileList =
+            filesProp.split(",")
+                .filter { it.isNotBlank() }
+                .map { file(it) }
+        source.setFrom(fileList)
+    } else {
+        source.setFrom(files("src/main/kotlin"))
+    }
+}
+
+tasks.register("copyGitHooks") {
+    doLast {
+        val sourceDir = file("hooks")
+        val targetDir = file(".git/hooks")
+        sourceDir.listFiles()
+            ?.forEach { sourceFile ->
+                val targetFile = File(targetDir, sourceFile.name)
+                if (!targetFile.exists() ||
+                    Files.mismatch(sourceFile.toPath(), targetFile.toPath()) != -1L
+                ) {
+                    Files.copy(
+                        sourceFile.toPath(),
+                        targetFile.toPath(),
+                        StandardCopyOption.REPLACE_EXISTING,
+                    )
+                    print("> Copied hook: ${sourceFile.name}")
+                }
+            }
+    }
+    doLast {
+        file(".git/hooks/").walk()
+            .forEach { file ->
+                if (file.isFile) {
+                    try {
+                        Files.setPosixFilePermissions(
+                            file.toPath(),
+                            PosixFilePermissions.fromString("rwxr-xr-x"),
+                        )
+                    } catch (_: UnsupportedOperationException) {
+                        logger.warn("Unable to set POSIX permissions on ${file.name}.")
+                    }
+                }
+            }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Santoro"
 include(":app")
- 
