d21f45f7bf3be0e9b72215373589b70284494de5
asensiodev <asensiodev@gmail.com>
2025-12-02 19:15:22 +0100

    Add login module

---
+++ feature/login/impl/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.convention.android.feature)
+    alias(libs.plugins.convention.paparazzi)
+}
+
+android {
+    namespace = "com.asensiodev.feature.login.impl"
+}
+
+dependencies {
+}
