9c4c08dd38f761886e7f3de95a1f9f8f626dc711
asensiodev <asensiodev@gmail.com>
2026-04-03 18:43:49 +0200

    build: include native debug symbols in release bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,9 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
         }
     }
 
